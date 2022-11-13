db83e52a024808825b492c6f6f588ca53263484e
Paolo Pedrigal <paolopedrigal@gmail.com>
2022-11-12 17:17:05 -0800

    I put the Node class in its own JS file and the utility functions in its own JS file

--- canvas.js
+++ canvas.js
@@ -1,88 +1,53 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+import {randomInt, randomColor, distance } from "./utils.js";
+import Node from "./node.js";
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+const canvas = document.querySelector('canvas'); // canvas object from HTML
+const c = canvas.getContext('2d'); // this provides the actual context/area for the canvas
 
+canvas.width = innerWidth; // innerWidth is the width of the browser of the window viewport (area being currently viewed)
+canvas.height = innerHeight; // innerHeight is the height of the browser of the window viewport (area being currently viewed)
 
-const mouse = {
+const mouse = { // initializes mouse's position in the center of the screen
   x: innerWidth / 2,
   y: innerHeight / 2
-}
+};
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
 
 // Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
+addEventListener('mousemove', (event) => { // updates the mouse's x/y coordinates
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
 })
 
-addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
+addEventListener('resize', () => { // updates the canvas's screen when the browser is resized
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
 
-  init()
+  // init() //   <--- Potentially uncomment this
 })
 
-// Utility Functions
-function randomIntFromRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-  
-  function randomColor(colors) {
-    return colors[Math.floor(Math.random() * colors.length)]
-  }
-  
-  function distance(x1, y1, x2, y2) {
-    const xDist = x2 - x1
-    const yDist = y2 - y1
-  
-    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
-  }
-
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-  }
-
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-  }
-
-  update() {
-    this.draw()
-  }
-}
 
 // Implementation
-let objects
+let node1;
+let node2;
 function init() {
-  objects = []
+  node1 = new Node(300, 300, 100, "black", c);
+  node2 = new Node(undefined, undefined, 30, "red", c);
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
-  }
 }
+
 
 // Animation Loop
 function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+  requestAnimationFrame(animate) // Calls itself to frequently animate the canvas
+  c.clearRect(0, 0, canvas.width, canvas.height) // Resets the canvas for the next animation
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+  node1.draw();
+  node2.x = mouse.x;
+  node2.y = mouse.y;
+  node2.draw();
 }
 
-init()
-animate()
+init();
+animate();
---
+++ node.js
@@ -0,0 +1,18 @@
+export default class Node {
+    constructor(x, y, radius, color, context) {
+      this.x = x;
+      this.y = y;
+      this.radius = radius;
+      this.color = color;
+      this.context = context;
+    }
+  
+    draw() {
+      this.context.beginPath();
+      this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      this.context.fillStyle = this.color;
+      this.context.fill();
+      this.context.closePath();
+    }
+  
+}
